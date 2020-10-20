3700e3168ea0d84284a4540bc724dcce03569c9e
Arvind <varvind@tamu.edu>
2020-10-20 14:59:06 -0500

    added fucntionality to query 10 tweets and am currently working on parsing urls from the tweets

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,6 +1,28 @@
-
-module.exports = (req, res) => {
-  res.render('index', {
-    layout: false
+const twitter = require('../controllers/twitterAPI')
+var tweets = [];
+module.exports = async (req, res) => {
+twitter.getTweets().then((response) => {
+    response.data.forEach(tweet => {
+      var text = tweet.text
+      var imgs = []
+      while(text.includes('https')) {
+        var url = "";
+        for(var i = text.indexOf("https"); i < text.length; i++) {
+          if(text.charAt(i) == ' ') {
+            break
+          }
+          url += text[i]
+        }
+        text = text.replace(url, " ");
+        imgs.push(url)
+      }
+      console.log(text)
+      console.log(imgs)
+      tweets.push({ text: text , img:imgs })
+    });
+    res.render('index', {
+      layout: false,
+      tweets
+    })
   })
 }
