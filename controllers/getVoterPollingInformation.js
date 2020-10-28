540d2ac14556e59f7c16bbca1df52b39d0a7299c
Arvind <varvind@tamu.edu>
2020-10-28 11:36:53 -0500

    added ability to query polling locations based off of an address

---
+++ controllers/getVoterPollingInformation.js
@@ -0,0 +1,51 @@
+require('dotenv').config({path: __dirname + '/../.env'});
+const { response } = require('express');
+const needle = require('needle');
+// The code below sets the bearer token from your environment variables
+// To set environment variables on Mac OS X, run the export command below from the terminal: 
+// export BEARER_TOKEN='YOUR-TOKEN' 
+const key = process.env.GOOGLE_KEY; 
+const endpoint = 'https://www.googleapis.com/civicinfo/v2/voterinfo'
+var endpointUrl = ''
+
+
+async function getRequest() {
+    endpointUrl = endpoint
+    // Edit query parameters below
+    const params = {
+        'key': key,
+        'address': '711 University Dr, College Station, TX 77840'
+    } 
+
+    const res = await needle('get', endpointUrl, params, { headers: {
+        "authorization": ''
+    }})
+    if(res.body) {
+        return res.body;
+    } else {
+        throw new Error ('Unsuccessful request')
+    }
+}
+
+async function getPollingLocation() {
+    endpointUrl = ''
+    try {
+        // Make request
+        const response = await getRequest();
+        if(response.pollingLocations == undefined) {
+          return new Array()
+        } else {
+          return response.pollingLocations
+        }
+        
+        
+    } catch(e) {
+        console.log(e);
+        return []
+        process.exit(-1);
+    }
+    process.exit();
+}
+
+getPollingLocation();
+module.exports.getPollingLocation = getPollingLocation;
--- controllers/homePage.js
+++ controllers/homePage.js
@@ -1,54 +1,65 @@
 const tweetGrabber = require('./getRecentTweets')
 const urlGrabber = require('./getTweetUrl')
 const htmlGrabber = require('./getTweetHTML')
+const pollLocationGrabber = require('./getVoterPollingInformation')
 const e = require('express')
 
 module.exports = (req, res) => {
   var tweetHTML = []
   var tweetIds = []
-  var prom = new Promise((res, rej) => {
-    const tweets = tweetGrabber.getTweets().then((response) => {
-      response.data.forEach(tweet => {
-        var id = tweet.id
-        tweetIds.push(id)
-      })
-      res()
+  var pollingLocations = []
+  var getPollingLocations = new Promise((res, rej) => {
+    pollLocationGrabber.getPollingLocation().then((response)=> {
+      pollingLocations = response
     })
-
+    res()
   })
-  prom.then((result) => {
-    promises = []
-    tweetIds.forEach((tweetid) => {
-      var prom1 = new Promise((res, rej) => {
-        const url = urlGrabber.getTweetUrl(tweetid).then((response) => {
-          var newUrl = response
-          if(newUrl.includes('photo')) {
-            newUrl = newUrl.substr(0, newUrl.indexOf('photo') -1)
-          } else if (newUrl.includes('video')) {
-            newUrl = newUrl.substr(0, newUrl.indexOf('video') -1)
-          }
-          if(newUrl != '') {
-            const prom2 = new Promise((res, rej) => {
-              const html = htmlGrabber.getTweetHTML(newUrl).then((html) => {
-                if(html != undefined) {
-                  tweetHTML.push(html)
-                }
+  getPollingLocations.then((result) => {
+    var prom = new Promise((res, rej) => {
+      const tweets = tweetGrabber.getTweets().then((response) => {
+        response.data.forEach(tweet => {
+          var id = tweet.id
+          tweetIds.push(id)
+        })
+        res()
+      })
+    })
+    prom.then((result) => {
+      promises = []
+      tweetIds.forEach((tweetid) => {
+        var prom1 = new Promise((res, rej) => {
+          const url = urlGrabber.getTweetUrl(tweetid).then((response) => {
+            var newUrl = response
+            if(newUrl.includes('photo')) {
+              newUrl = newUrl.substr(0, newUrl.indexOf('photo') -1)
+            } else if (newUrl.includes('video')) {
+              newUrl = newUrl.substr(0, newUrl.indexOf('video') -1)
+            }
+            if(newUrl != '') {
+              const prom2 = new Promise((res, rej) => {
+                const html = htmlGrabber.getTweetHTML(newUrl).then((html) => {
+                  if(html != undefined) {
+                    tweetHTML.push(html)
+                  }
+                  res()
+                })
+              }).then((result) => {
                 res()
               })
-            }).then((result) => {
+            } else {
               res()
-            })
-          } else {
-            res()
-          }
+            }
+          })
         })
+        promises.push(prom1)
       })
-      promises.push(prom1)
-    })
-    Promise.all(promises).then((result) => {
-      res.render('index', {
-        layout: false,
-        tweetHTML
+      Promise.all(promises).then((result) => {
+        console.log(pollingLocations)
+        res.render('index', {
+          layout: false,
+          tweetHTML,
+          pollingLocations
+        })
       })
     })
 
